83e3de135d4414424e65304f2d75f3e343432af4
mmcnicol <matthew.mcnicol@gmail.com>
2023-10-31 22:20:35 +0000

    fixed a broken test

--- city_test.go
+++ city_test.go
@@ -75,29 +75,3 @@ func TestManufactureUnit(t *testing.T) {
 		t.Errorf("day %d, occupied city, value of DaysUntilUnitReady = %d; want %d", day, city.DaysUntilUnitReady, 4)
 	}
 }
-
-func TestGetDaysToProduceUnit(t *testing.T) {
-	type test struct {
-		name string
-		unit UnitType
-		want int
-	}
-	tests := []test{
-		{name: "Tank", unit: Tank, want: 4},
-		{name: "Fighter", unit: Fighter, want: 6},
-		{name: "Bomber", unit: Bomber, want: 25},
-		{name: "Transport", unit: Transport, want: 8},
-		{name: "Destroyer", unit: Destroyer, want: 8},
-		{name: "Submarine", unit: Submarine, want: 8},
-		{name: "Carrier", unit: Carrier, want: 10},
-		{name: "Battleship", unit: Battleship, want: 20},
-	}
-	c := NewCity(0, 0)
-	for _, tc := range tests {
-		got := c.GetDaysToProduceUnit(tc.unit)
-		//if !reflect.DeepEqual(err, tc.want) {
-		if got != tc.want {
-			t.Fatalf("c.GetDaysToProduceUnit(), name:%s, expected: %d, got: %d", tc.name, tc.want, got)
-		}
-	}
-}
--- unit_test.go
+++ unit_test.go
@@ -29,3 +29,29 @@ func TestGetNewUnitStrength(t *testing.T) {
 		}
 	}
 }
+
+func TestGetDaysToProduceUnit(t *testing.T) {
+	type test struct {
+		name string
+		unit UnitType
+		want int
+	}
+	tests := []test{
+		{name: "Tank", unit: Tank, want: 4},
+		{name: "Fighter", unit: Fighter, want: 6},
+		{name: "Bomber", unit: Bomber, want: 25},
+		{name: "Transport", unit: Transport, want: 8},
+		{name: "Destroyer", unit: Destroyer, want: 8},
+		{name: "Submarine", unit: Submarine, want: 8},
+		{name: "Carrier", unit: Carrier, want: 10},
+		{name: "Battleship", unit: Battleship, want: 20},
+	}
+	//c := NewCity(0, 0)
+	for _, tc := range tests {
+		got := GetDaysToProduceUnit(tc.unit)
+		//if !reflect.DeepEqual(err, tc.want) {
+		if got != tc.want {
+			t.Fatalf("c.GetDaysToProduceUnit(), name:%s, expected: %d, got: %d", tc.name, tc.want, got)
+		}
+	}
+}
